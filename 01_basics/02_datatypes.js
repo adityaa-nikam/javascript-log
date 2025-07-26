23275c283e2fd280152b493b192d1925a146d9cb
aditya nikam <adiityaa.nikam@gmail.com>
2025-07-26 22:33:04 +0000

    learnt datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";  // treat all the JS code as newer version
+
+// alert( 3 + 3 )  // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3);   // code readability should be high
+
+     // documentaion to be use used for learning JS
+     // - MDN DOCS / tc39 (ecma script)
+
+let name = "Aditya"
+let age = 20
+let isLoggedIn = false
+
+// number  ==>  2 to power 53
+// bigint  
+// string  ==> ""
+// boolean  ==> true/false
+// null  ==> standalone value
+// undefined  ==> when no value is defined
+// symbol  ==> unique
+
+// object 
+
+console.log(typeof "Aditya");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);  //  object
