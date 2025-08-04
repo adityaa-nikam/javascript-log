b5c1be8fb7eb8b554f4fe9d86d87754ee3a09aa3
aditya nikam <adiityaa.nikam@gmail.com>
2025-08-04 18:06:45 +0000

    learned comparsion

---
+++ 04_comparison.js
@@ -0,0 +1,24 @@
+// console.log( 2 > 1 );
+// console.log( 2 >= 1 );
+// console.log( 2 < 1 );
+// console.log( 2 == 1 );
+// console.log( 2 != 1 );
+
+// console.log( "2" > 1 );
+// console.log( "02" > 1 );
+
+// console.log( null > 0 );
+// console.log( null == 0 );
+// console.log( null >= 0 );
+
+// console.log( undefined == 0 );
+// console.log( undefined > 0 );
+// console.log( undefined < 0 );  // we avoid these conversions mostly
+
+// ===  strict check
+
+console.log("2" === 2);
+
+
+
+
